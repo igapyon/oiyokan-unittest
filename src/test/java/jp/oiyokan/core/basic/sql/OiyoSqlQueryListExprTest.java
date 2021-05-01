a40e3e942f590327b4978cebaa187ca2d7f7c775
Toshiki Iga <igapyon@gmail.com>
2021-05-01 20:09:01 +0900

    h2 test

--- src/test/java/jp/oiyokan/core/basic/sql/OiyoSqlQueryListExprTest.java
+++ src/test/java/jp/oiyokan/core/basic/sql/OiyoSqlQueryListExprTest.java
@@ -105,7 +105,7 @@ class OiyoSqlQueryListExprTest {
             assertEquals("((STRPOS(Description,?) - 1) <> -1)", getExprString("/ODataTest1", //
                     OiyoUrlUtil.encodeUrlQuery(
                             "$top=51&$filter= indexof(Description,'増殖タブレット7') ne -1 &$orderby=ID &$count=true &$select=Description,ID,Name")),
-                    "postgres");
+                    "PostgreSQL");
             break;
         case SQLSV2008:
             assertEquals("((CHARINDEX(?,Description) - 1) <> -1)", getExprString("/ODataTest1", //
