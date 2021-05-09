b96d60873ccb7630fee07f75e6b7f3b87ac414fd
Toshiki Iga <igapyon@gmail.com>
2021-05-09 18:48:34 +0900

    version increment.

--- src/main/java/jp/oiyokan/demosite/OiyokanDemositeConstants.java
+++ src/main/java/jp/oiyokan/demosite/OiyokanDemositeConstants.java
@@ -19,5 +19,5 @@ package jp.oiyokan.demosite;
  * Oiyokan Demosite の定数.
  */
 public class OiyokanDemositeConstants {
-    public static final String VERSION = "1.11.20210503b";
+    public static final String VERSION = "1.11.20210509a";
 }
