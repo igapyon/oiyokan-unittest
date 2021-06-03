dd1f9e2b6baadb9ce85d6e0bed7878b11b851b20
Toshiki Iga <igapyon@gmail.com>
2021-06-03 20:06:11 +0900

    update lib version.

--- src/main/java/jp/oiyokan/unittest/OiyokanUnittestConstants.java
+++ src/main/java/jp/oiyokan/unittest/OiyokanUnittestConstants.java
@@ -19,5 +19,5 @@ package jp.oiyokan.unittest;
  * Oiyokan UnitTest の定数.
  */
 public class OiyokanUnittestConstants {
-    public static final String VERSION = "1.17.20210525a";
+    public static final String VERSION = "1.19.20210603a";
 }
